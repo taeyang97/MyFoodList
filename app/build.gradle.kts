3f4f2154951778ac5180d51bc55a6efba213cd46
Taeyang <zlaxodid1@naver.com>
2023-02-22 23:31:24 +0900

    build: migration version catalogs with toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,17 +28,17 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 }
 
 dependencies {
 
-    implementation ("androidx.core:core-ktx:1.7.0")
-    implementation ("androidx.appcompat:appcompat:1.6.0")
-    implementation ("com.google.android.material:material:1.8.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    implementation (libs.androidx.core.ktx)
+    implementation (libs.androidx.appcompat)
+    implementation (libs.android.material)
+    implementation (libs.androidx.constraintlayout)
+    testImplementation (libs.test.junit4)
+    androidTestImplementation (libs.androidx.test.junit4.ext)
+    androidTestImplementation (libs.androidx.test.espresso.core)
 }
